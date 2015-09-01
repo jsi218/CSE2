d3b3bf1b2f2ed195cb93c158d0704dbfc6bd13a4
jsi218 <jsi218@lehigh.edu>
2015-09-01 19:47:13 +0000

    updated WelcomeClass

---
+++ hw01-WelcomeClass/WelcomeClass.java
@@ -0,0 +1,28 @@
+//Brian Chen
+//Demo 0
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the statement with the box around it
+    System.out.println("-----------");
+    System.out.println("  WELCOME  ");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-j--s--i--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("Last week on August 27 was my birthday and I turned 20 years old.");
+    
+    }
+}
